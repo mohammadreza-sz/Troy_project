9233425ee0b6d95d5d2e3f68a12da39948b1ef7e
mohammadreza-sz <ohm.sahebzade@gmail.com>
2023-03-29 18:46:50 +0430

    add apps and database config

--- Troy/settings.py
+++ Troy/settings.py
@@ -92,12 +92,15 @@ WSGI_APPLICATION = 'Troy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.mysql',
+   #     'NAME': 'Troy',
+    #    'HOST': 'localhost',
+     #   'USER': 'root',
+      #  'PASSWORD': 'MRS'
+    #}
+#}
 
 
 # Password validation
