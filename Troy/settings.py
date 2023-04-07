1bfc15346201851ac3fdff189f9f501c8c5460f3
mohammadreza-sz <ohm.sahebzade@gmail.com>
2023-04-07 15:32:17 +0430

    change database

--- Troy/settings.py
+++ Troy/settings.py
@@ -95,17 +95,22 @@ WSGI_APPLICATION = 'Troy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': 'Troy',
+#         # 'HOST': 'localhost',
+#         # 'USER': 'root',
+#         # 'PASSWORD': 'Mrsmysql18!',
+#         # 'PORT':'3306'
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Troy',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'Mrsmysql18!',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
-
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
