7af15b1e869c8b266272c9980307919532c4fbc7
khoshdoost <frnz.azad2002@gmail.com>
2023-05-06 16:57:17 +0430

    config and fix messages without room

--- chat/models.py
+++ chat/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
+# from acount.models import Person
+from Troy.settings import AUTH_USER_MODEL
+# User = get_user_model()
+User = AUTH_USER_MODEL
 # we must modify it...
-class Contact(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    friends = models.ManyToManyField('self', blank = True)
+# class Contact(models.Model):
+#     user = models.ForeignKey(User, on_delete = models.CASCADE)
+#     friends = models.ManyToManyField('self', blank = True)
 
-    def __str__(self):
-        return self.user.username
+#     def __str__(self):
+#         return self.user.username
 
 
-class Message(models.Model):
-    contact = models.ForeignKey(Contact, related_name='messages', on_delete= models.CASCADE)
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add = True)
+# class Message(models.Model):
+#     contact = models.ForeignKey(Contact, related_name='messages', on_delete= models.CASCADE)
+#     content = models.TextField()
+#     timestamp = models.DateTimeField(auto_now_add = True)
 
-    def __str__(self): 
-        return self.contact.user.username
+#     def __str__(self): 
+#         return self.contact.user.username
 
 class Chat(models.Model):
     participants = models.ManyToManyField(Contact, related_name= 'chats')
