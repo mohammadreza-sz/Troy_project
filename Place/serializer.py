e4a58fac4af3b1d279fa89f9193ad462e5088f59
khoshdoost <frnz.azad2002@gmail.com>
2023-05-07 12:29:41 +0430

    add rate_no in serializers

--- Place/serializer.py
+++ Place/serializer.py
@@ -1,49 +1,98 @@
-
-# from ..Profile.serializers import CitySerializer
-from dataclasses import fields
-from .models import Place , PlaceImage, Rate
-from rest_framework import serializers
-class PlaceImageSerializer(serializers.ModelSerializer):#mrs
-    class Meta:
-        model = PlaceImage
-        fields =['id' , 'image' , 'place_id']
-
-
-class PlaceSerializer(serializers.ModelSerializer):#mrs 59
-    # placeimage_set = PlaceImageSerializer(many = True , read_only = True)
-    # placeimage_set = serializers.SlugRelatedField(
-    #     many=True,
-    #     read_only=True,
-    #     slug_field='image'
-    # )
-    # city = CitySerializer()
-    # city_id = serializers.CharField(max_length = 50)
-    # country_id= serializers.CharField(max_length = 50)
-    avg_rate = serializers.ReadOnlyField()
-
-    class Meta:
-        model = Place
-        fields = [
-                # 'country_name',
-                # 'city_id__city_name',
-                # 'city_id',           
-                'id',
-                'name',
-                'address',
-                'description',
-                'lan',
-                'lon',
-                # 'placeimage',
-                'avg_rate',
-
-            ]
-
-
-class RateSerializer(serializers.ModelSerializer):#mrs 59
-    class Meta:
-        model = Rate
-        fields = [
-            'place',
-            'user',
-            'rate'
+
+
+# from ..Profile.serializers import CitySerializer
+
+from dataclasses import fields
+
+from .models import Place , PlaceImage, Rate
+
+from rest_framework import serializers
+
+class PlaceImageSerializer(serializers.ModelSerializer):#mrs
+
+    class Meta:
+
+        model = PlaceImage
+
+        fields =['id' , 'image' , 'place_id']
+
+
+
+
+
+class PlaceSerializer(serializers.ModelSerializer):#mrs 59
+
+    # placeimage_set = PlaceImageSerializer(many = True , read_only = True)
+
+    # placeimage_set = serializers.SlugRelatedField(
+
+    #     many=True,
+
+    #     read_only=True,
+
+    #     slug_field='image'
+
+    # )
+
+    # city = CitySerializer()
+
+    # city_id = serializers.CharField(max_length = 50)
+
+    # country_id= serializers.CharField(max_length = 50)
+
+    avg_rate = serializers.ReadOnlyField()
+
+
+
+    class Meta:
+
+        model = Place
+
+        fields = [
+
+                # 'country_name',
+
+                # 'city_id__city_name',
+
+                # 'city_id',           
+
+                'id',
+
+                'name',
+
+                'address',
+
+                'description',
+
+                'lan',
+
+                'lon',
+
+                # 'placeimage',
+                'rate_no',
+
+                'avg_rate',
+
+
+
+            ]
+
+
+
+
+
+class RateSerializer(serializers.ModelSerializer):#mrs 59
+
+    class Meta:
+
+        model = Rate
+
+        fields = [
+
+            'place',
+
+            'user',
+
+            'rate'
+
         ]
